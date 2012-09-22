42b5127ed62c74228caa6ce38fa55a0e72f798f9
bluk44 <bluk44@gmail.com>
2012-09-22 16:02:14 +0200

    sprawdzarka ver 1

--- PA2011/src/tools/Sprawdzarka.java
+++ PA2011/src/tools/Sprawdzarka.java
@@ -6,28 +6,55 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Sprawdzarka {
-	
-	public void check(Task task, File testDir) throws FileNotFoundException{
-		//HashMap<String, File> inputFiles = new HashMap<String, File>(), outputFiles = new HashMap<String, File>();
-		FileInputStream ansIn = new FileInputStream("testy/PAL/test0.out");
-		FileInputStream testIn = new FileInputStream("testy/PAL/test0.in");
-		byte[] answer = readAnswer(ansIn);
-		byte[] result = task.solve(testIn);
-		
-		String sAns = new String(answer);
-		String sRes = new String(result);
-		
-		if(isCorrect(result, answer)){
-			println("OK: "+sAns+" ["+sRes+"]");
-		} else{
-			println("FAIL: "+sAns+" ["+sRes+"]");
+
+	public void check(Task task, File testDir) throws FileNotFoundException {
+		HashMap<String, File> inputFiles = new HashMap<String, File>(), outputFiles = new HashMap<String, File>();
+		ArrayList<String> testNames = new ArrayList<String>();
+
+		if (testDir.isDirectory()) {
+			File[] files = testDir.listFiles();
+			for (File f : files) {
+				String pathName = f.getPath();
+				if (pathName.endsWith("in")) {
+					int dot = pathName.indexOf(".");
+					inputFiles.put(pathName.substring(0, dot), f);
+					testNames.add(pathName.substring(0, dot));
+
+				} else if (pathName.endsWith("out")) {
+					int dot = pathName.indexOf(".");
+					outputFiles.put(pathName.substring(0, dot), f);
+				}
+			}
 		}
-		
+		String[] names = new String[testNames.size()];
+		testNames.toArray(names);
+		Arrays.sort(names);
+
+		for (int i = 0; i < names.length; i++) {
+			FileInputStream ansIn = new FileInputStream(names[i] + ".out");
+			FileInputStream testIn = new FileInputStream(names[i] + ".out");
+			println(names[i] + ": ");
+			byte[] answer = readAnswer(ansIn);
+			byte[] result = task.solve(testIn);
+
+			String sAns = new String(answer);
+			String sRes = new String(result);
+
+			if (isCorrect(result, answer)) {
+				System.out.println("OK: "+ sAns + "RESULT: "+ sRes);
+			} else {
+				println("FAIL: "+ sAns + sRes);
+			}
+		}
 	}
-	
-	private byte[] readAnswer(InputStream in){
+
+	private byte[] readAnswer(InputStream in) {
 		ByteArrayOutputStream ansOut = new ByteArrayOutputStream();
 		byte b = 0;
 		try {
@@ -41,26 +68,54 @@ public class Sprawdzarka {
 		}
 		return null;
 	}
-	
-	private boolean isCorrect(byte[] b1, byte[] b2){
-		if(b1.length != b2.length){
+
+	private boolean isCorrect(byte[] b1, byte[] b2) {
+		if (b1.length != b2.length) {
 			return false;
-		}
-		else{
-			for(int i=0;i<b2.length;i++){
-				if(b1[i] != b2[i]){
+		} else {
+			for (int i = 0; i < b2.length; i++) {
+				if (b1[i] != b2[i]) {
 					return false;
 				}
 			}
 		}
 		return true;
 	}
-	
-	private void println(String s){
+
+	private void println(String s) {
 		System.out.println(s);
 	}
-	
-	public static void main(String[] args){
 
- 	}
+	public static void main(String[] args) {
+		Sprawdzarka spr = new Sprawdzarka();
+		Chuj chuj = new Chuj();
+		try {
+			spr.check(chuj, new File("testy/PAL"));
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
+
+class Chuj implements Task {
+
+	@Override
+	public byte[] solve(InputStream in) {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+		byte b = 0;
+		try {
+			while ((b = (byte) in.read()) != -1) {
+				out.write(b);
+			}
+			return out.toByteArray();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		return out.toByteArray();
+	}
+
 }
--- PA2011/src/tools/Task.java
+++ PA2011/src/tools/Task.java
@@ -3,7 +3,7 @@ package tools;
 import java.io.InputStream;
 
 public interface Task {
-	
+		
 	byte[] solve(InputStream in);
 	
 }
